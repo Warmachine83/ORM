23e04767771fc394400724aac294d4a661fbc8ec
romain <romain@romainLaravel.local>
2020-04-09 17:32:26 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
 
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+/*
 Route::get('/', function(){
 	return view('users');
 });
+*/
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
